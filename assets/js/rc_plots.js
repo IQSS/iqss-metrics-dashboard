7ead4c4cf00af88b732ba609714d719b36a37ca0
Erik Buunk <erik_buunk@iq.harvard.edu>
2020-06-22 15:27:32 -0400

    add RCE

---
+++ assets/js/rc_plots.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    generateRCPlot(path, 'interactive', 'department');
+    generateRCPlot(path, 'interactive', 'school');
+    generateRCPlot(path, 'batch', 'department');
+    generateRCPlot(path, 'batch', 'school');
+});
+
+function generateRCPlot(path, name, type) {
+    let div = `rc-${name}-by-${type}`
+    d3.tsv(path + div + ".tsv", function (data) {
+
+        console.log(div)
+        let labels = []
+        let values = []
+        let year = data[0]["year"];
+        let title = "RCE " + name + "by" + type + " in " + year // not used
+
+        for (i = 0; i < data.length; i++) {
+            labels.push(`${data[i][type]} (${data[i]["percentage"]})`);
+            values.push(data[i]["count"]);
+        }
+
+        let dataset = {
+            x: labels,
+            y: values,
+            title: title
+        }
+
+        console.log(dataset)
+        pieChart(div, dataset);
+        document.getElementById(div + "-year").innerHTML = year;
+    })
+}

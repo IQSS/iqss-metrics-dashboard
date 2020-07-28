ad0757c114d84e1fae442d72aa14389fb098e78d
Erik Buunk <erik_buunk@iq.harvard.edu>
2020-07-28 14:24:25 -0400

    First version DSS

--- assets/js/dss_plots.js
+++ assets/js/dss_plots.js
@@ -1,31 +1,24 @@
+// D3 help for this version 
+// https: //github.com/alexandersimoes/d3plus/wiki/Visualizations
+
 // Run after DOM is loaded
-let path =
-    "assets/data/dss/"
+let path = "assets/data/dss/";
+let fontFamily = "Montserrat";
+
+
+
 $(document).ready(function () {
-    patron_community();
+    year_quarter(); //1
+    timeSeriesCommunity(); //2
+    patron_community(); //3
     request_type();
-    year_quarter();
-    timeSeriesCommunity();
     workshopAttendance();
-    workshopNumber();
-    dataFestAttendance();
-    dataFestNumber();
-    resOutputDepts();
-    resOutputYears();
+    // workshopNumber();
+    // dataFestAttendance();
+    // dataFestNumber();
+    // resOutputDepts();
+    // resOutputYears();
 });
-
-
-// Coerce data values to be numeric (one input)
-// function coerceToNum(data, variable){
-//     data.forEach(function(d) {
-//         d3.keys(d).forEach(function(k){
-//             if(k == variable){
-//                 d[k] = +d[k]
-//             }
-//         })
-//     });
-//     return data;
-// };
 
 // Coerce data values to be numeric (multiple inputs)
 function coerceToNum(data, vararray) {
@@ -51,118 +44,67 @@ function coerceToNum(data, vararray) {
     return data;
 };
 
-
-// Patron Community
-function patron_community() {
-    d3.csv(path + "community.csv", function (error, data) {
-        if (error) return console.error(error);
-
-        // Coerce data values to be numeric
-        var myNumData = coerceToNum(data, "n");
-
-        // Visualize
-        var visualization = d3plus.viz()
-            .container(".patron_community")
-            .data(myNumData)
-            .type("bar")
-            .id("patron_community")
-            .y({
-                "value": "patron_community",
-                "scale": "discrete",
-                "label": "Affiliation"
-            })
-            .x({
-                "value": "n",
-                "label": "Number of Help Requests by Affiliation"
-            })
-            .order({
-                "sort": "asc",
-                "value": "n"
-            })
-            .color("col")
-            .legend(false)
-            .draw()
-    });
-};
-
-
-// Request Type
-function request_type() {
-    d3.csv(path + "request_types.csv", function (error, data) {
-        if (error) return console.error(error);
-
-        // Coerce data values to be numeric
-        var myNumData = coerceToNum(data, "n");
-
-        var visualization = d3plus.viz()
-            .container(".request_type")
-            .data(myNumData)
-            .type("bar")
-            .id("request_type")
-            .y({
-                "value": "request_type",
-                "scale": "discrete",
-                "label": "Type of Help Requested"
-            })
-            .x({
-                "value": "n",
-                "label": "Number of Help Requests by Type"
-            })
-            .order({
-                "sort": "asc",
-                "value": "n"
-            })
-            .color("colRs")
-            .legend(false)
-            .draw()
-    });
-};
-
-// Quarter
+// Quarter #OK (1)
 function year_quarter() {
     d3.csv(path + "year_quarter.csv", function (error, data) {
         if (error) return console.error(error);
 
         // Coerce data values to be numeric
-        var myNumData = coerceToNum(data, "n");
+        var myNumData = coerceToNum(data, ["n", "year"]);
 
-        var visualization = d3plus.viz()
+        d3plus.viz()
             .container(".year_quarter")
             .data(myNumData)
             .type("bar")
             .id("year")
-            .y({
+            .x({
                 "value": "yearQuarter",
                 "scale": "discrete",
                 "label": "Year \& Quarter"
             })
-            .x({
+            .y({
                 "value": "n",
                 "label": "Number of New Help Requests"
             })
-            .color("year")
-            .legend({
-                "order": {
-                    "sort": "desc",
-                    "value": "size"
-                },
-                "size": 35,
-                "text": "year"
+            .color(d =>
+                iqss_color_pallette[(new Date().getFullYear() - parseInt(d.year)) % iqss_color_pallette.length]
+            )
+            // .legend({
+            //     "order": {
+            //         "sort": "asc",
+            //         "value": "year"
+            //     },
+            //     "size": 50,
+            //     // "text": "year"
+            // })
+            .legend(false)
+            .font({
+                "family": fontFamily
             })
+            .height({
+                "max": false
+            })
+            .resize(true)
             .draw()
     });
 };
 
-//
-
+//#OK (2)
 function timeSeriesCommunity() {
     d3.csv(path + "timeseries_community_quarter.csv", function (error, data) {
         if (error) return console.error(error);
 
         // Coerce data values to be numeric
         var myNumData = coerceToNum(data, "cumulative_tickets");
-
-        var visualization = d3plus.viz()
+        // console.log(myNumData)
+        d3plus.viz()
+            // .title({
+            //     "value": "Cumulative Number of Requests (Top Users)",
+            //     "position": "bottom",
+            //     "font": {
+            //         "size": "12px"
+            //     }
+            // })
             .container(".timeseriesCommunity")
             .data(myNumData)
             .type("line")
@@ -175,31 +117,127 @@ function timeSeriesCommunity() {
                 "value": "quarter",
                 "label": "Quarter"
             })
-            .title({
-                "value": "Cumulative Number of Requests (Top Users)",
-                "position": "bottom",
-                "font": {
-                    "size": "12px"
-                }
+            .color("patron_community")
+            .legend({
+                "order": {
+                    "sort": "asc",
+                    "value": "patron_community"
+                },
+                "size": 75,
+                // "text": "patron_community",
+                // "filters": true,
+                // "icons": true,
+
             })
+            .font({
+                "family": fontFamily
+            })
+            .resize(true)
             //.time({"value": "quarter"})
             .draw();
     });
 };
 
+// Patron Community (3)
+function patron_community() {
+    d3.csv(path + "community.csv", function (error, data) {
+        if (error) return console.error(error);
+
+        // Coerce data values to be numeric
+        var myNumData = coerceToNum(data, "n");
+        data_aggr = create_rest_category(myNumData, 0.005, "n", "patron_community")
+
+        // Visualize
+        d3plus.viz()
+            .container(".patron_community")
+            .data(data_aggr)
+            .type("bar")
+            .id("patron_community")
+            .y({
+                "value": "patron_community",
+                "scale": "discrete",
+                "label": "Affiliation"
+            })
+            .x({
+                "value": "n",
+                "label": "Number of Help Requests"
+            })
+            .order({
+                "sort": "desc",
+                "value": "order"
+            })
+            .color(d => iqss_color_pallette[1])
+            .legend(false)
+            .font({
+                "family": fontFamily,
+                "transform": "none"
+            })
+            .resize(true)
+            .draw()
+    });
+};
+
+
+
+
+// Request Type
+function request_type() {
+    d3.csv(path + "request_types.csv", function (error, data) {
+        if (error) return console.error(error);
+
+        // Coerce data values to be numeric
+        var myNumData = coerceToNum(data, "n");
+
+        let data_aggr = create_rest_category(myNumData, 0.01, "n", "request_type")
+        console.log(data_aggr)
+
+        d3plus.viz()
+            .container(".request_type")
+            .data(data_aggr)
+            .type("bar")
+            .id("request_type")
+            .y({
+                "value": "request_type",
+                "scale": "discrete",
+                "label": "Type of Help Requested"
+            })
+            .x({
+                "value": "n",
+                "label": "Number of Help Requests"
+            })
+            .order({
+                "sort": "desc",
+                "value": "order"
+            })
+            .color(d => iqss_color_pallette[0])
+            .legend(false)
+            .font({
+                "family": fontFamily
+            })
+            .resize(true)
+            .draw()
+    });
+};
+
+
+
+//
+
+
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // WORKSHOPS
 
-// Regular workshops
+// Regular workshops (5)
 function workshopAttendance() {
-    d3.csv(path + "workshopAttendance.csv", function (error, data) {
+    d3.csv(path + "workshops.csv", function (error, data) {
         if (error) return console.error(error);
 
         // Coerce data values to be numeric
         var myNumData = coerceToNum(data, ["Attendance", "sortby"]);
 
-        var visualization = d3plus.viz()
+        d3plus.viz()
             .container(".workshopAttendance")
             .data(myNumData)
             .type("bar")
@@ -217,8 +255,12 @@ function workshopAttendance() {
                 "sort": "asc",
                 "value": "sortby"
             })
-            // .color("")
-            // .legend(false)
+            .color(d => iqss_color_pallette[0])
+            .legend(false)
+            .font({
+                "family": fontFamily
+            })
+            .resize(true)
             .draw()
     });
 };
@@ -230,7 +272,7 @@ function workshopNumber() {
         // Coerce data values to be numeric
         var myNumData = coerceToNum(data, ["Number", "sortby"]);
 
-        var visualization = d3plus.viz()
+        d3plus.viz()
             .container(".workshopNumber")
             .data(myNumData)
             .type("bar")
@@ -250,6 +292,10 @@ function workshopNumber() {
             })
             // .color("")
             // .legend(false)
+            .font({
+                "family": fontFamily
+            })
+            .resize(true)
             .draw()
     });
 };
@@ -282,6 +328,10 @@ function dataFestAttendance() {
             })
             // .color("")
             // .legend(false)
+            .font({
+                "family": fontFamily
+            })
+            .resize(true)
             .draw()
     });
 };
@@ -313,6 +363,10 @@ function dataFestNumber() {
             })
             // .color("")
             // .legend(false)
+            .font({
+                "family": fontFamily
+            })
+            .resize(true)
             .draw()
     });
 };
@@ -351,6 +405,10 @@ function resOutputDepts() {
             })
             // .color("")
             // .legend(false)
+            .font({
+                "family": fontFamily
+            })
+            .resize(true)
             .draw()
     });
 };
@@ -379,6 +437,10 @@ function resOutputYears() {
             // .order({"sort": "asc", "value": "sortby"})
             // .color("")
             // .legend(false)
+            .font({
+                "family": fontFamily
+            })
+            .resize(true)
             .draw()
     });
 };

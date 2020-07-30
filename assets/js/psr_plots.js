9f4faa02ca408edd8bf7f47d9bb24903081489e4
Erik Buunk <erik_buunk@iq.harvard.edu>
2020-07-30 13:58:41 -0400

    PSR added

---
+++ assets/js/psr_plots.js
@@ -0,0 +1,99 @@
+$(document).ready(function () {
+    console.log(path)
+    generatePSRPlots('school');
+    generatePSRPlots('role');
+});
+
+function titleCaseWord(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+}
+
+function generatePSRPlots(t) {
+    let div1 = `psr-${t}`
+    let div2 = `psr-${t}-distribution`
+    console.log(div1, div2)
+    let file = `psrAdvisesBy${titleCaseWord(t)}.tsv`
+    
+    d3.tsv(path + file, function (data) {
+
+        let labels = []
+        let values1 = []
+        let values2 = []
+
+        let year = data[0]["Period"];
+        console.log(year)
+
+
+        view = titleCaseWord(t)
+        let title = `Number of Advisees by ${view}`
+        
+
+        // Process TSV file
+        for (i = 0; i < data.length; i++) {
+            labels.push(`${data[i][view]}`);
+            values1.push(data[i]["Number of Advisees"]);
+            values2.push(data[i]["Cumulative Advisees"]);
+        }
+
+
+        // Mixed Chart
+        let datasets = []
+        let dataset1 = {
+            backgroundColor: iqss_blue,
+            borderColor: iqss_blue,
+            borderWidth: 4,
+            order: 1
+        }
+        let dataset2 = {        
+            backgroundColor: iqss_orange + '22',
+            borderColor: iqss_orange,
+            borderWidth: 4,
+            lineTension: 0,
+            order: 2}
+        
+        dataset1["label"] = "Number of Advisees"
+        dataset1["data"] = values1
+        
+        dataset2["label"] = "Cumulative"
+        dataset2["data"] = values2
+        dataset2["type"] = "line"
+
+
+        datasets.push(dataset1)
+        datasets.push(dataset2)
+
+        console.log(datasets)
+        console.log(labels)
+
+
+        let mixed_data_set ={
+            x: labels,
+            y: datasets,
+            title: title,
+            color: iqss_blue,
+            label_x: view,
+            label_y: "Number of Advisees"
+
+        }
+
+        mixedChart(div1, mixed_data_set)
+
+
+        // Pie Chart
+        let pie_data_set = {
+            x: labels,
+            y: values1,
+            title: title,
+            color: iqss_blue,
+            label_x: view,
+            label_y: "Number of Advisees"
+        }
+        pieChart(div2, pie_data_set);
+        elms = document.getElementsByClassName("psr-period")
+        for (e of elms) {
+            e.innerHTML = `(${year})`
+        }
+        // console.log(elms)
+        
+    })
+}

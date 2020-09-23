a89326310b68849df5d96c69b69ff4d338f5ea50
Erik Buunk <erik_buunk@iq.harvard.edu>
2020-09-23 14:48:28 -0400

    resize added

--- assets/js/css_plots.js
+++ assets/js/css_plots.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-    loadCSSPlots(path)
-    loadLabPlots(path)
+    resize();
+    loadCSSPlots(path);
+    loadLabPlots(path);
 });
 
 
--- assets/js/psr_plots.js
+++ assets/js/psr_plots.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    resize();
     generatePSRPlots('school');
     generatePSRPlots('role');
 });

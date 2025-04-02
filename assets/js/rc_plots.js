b36b260191f449437bcc1a58354b61ceae9d5413
Evan Sarmiento <evansarm@gmail.com>
2025-04-02 11:08:08 -0400

    Deploy corrected graphs

--- assets/js/rc_plots.js
+++ assets/js/rc_plots.js
@@ -1,34 +1,28 @@
+let rcDataPath = "assets/data/rc";
+
 $(document).ready(function () {
     resize();
-    generateRCPlot(path, 'interactive', 'department');
-    generateRCPlot(path, 'interactive', 'school');
-    generateRCPlot(path, 'batch', 'department');
-    generateRCPlot(path, 'batch', 'school');
+    //generateRCPlot(`${rcDataPath}/20251217FasrcPisDeptSocSciPis.tsv`);
+    generateRCPlots(`${rcDataPath}/20251217FasrcPisDeptSocSciUsageLab2024.tsv`, '.fassocrccpuxdept2024', "Dept", "CpuHours");
+    generateRCPlots(`${rcDataPath}/20251217FasrcPisDeptSocSciUsageLab2024.tsv`, '.fassocrcgpuxdept2024', "Dept", "GpuHours")
+
 });
 
-function generateRCPlot(path, name, type) {
-    let div = `rc-${name}-by-${type}`
-    d3.tsv(path + div + ".tsv", function (data) {
+function generateRCPlots(path, div, id, size) {
+    
+    d3.tsv(path, function (data) {
+        data.forEach(function(d) {
+            d.CpuHours = +d.CpuHours; // explicitly convert Num to numeric
+            d.GpuHours = +d.GpuHours;
+        });
 
-        // console.log(div)
-        let labels = []
-        let values = []
-        let year = data[0]["year"];
-        let title = "RCE " + name + "by" + type + " in " + year // not used
-
-        for (i = 0; i < data.length; i++) {
-            // labels.push(`${data[i][type]} (${data[i]["percentage"]})`);
-            labels.push(`${data[i][type]}`);
-            values.push(data[i]["count"]);
-        }
-
-        let dataset = {
-            x: labels,
-            y: values,
-            title: title
-        }
-
-        pieChart(div, dataset);
-        document.getElementById(div + "-period").innerHTML = `(${year})`;
-    })
-}
+        d3plus.viz()
+        .container(div)
+        .data(data)
+        .type("pie")
+        .id(id)
+        .size(size)
+        .legend(true)
+        .draw();
+    });
+};

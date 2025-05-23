0d2071c8b7dda2b41fc461c06e294ece9bc397c4
Evan Sarmiento <evansarm@gmail.com>
2025-05-23 10:43:57 -0400

    Add plots from 2025

--- assets/js/rc_plots_from_2025.js
+++ assets/js/rc_plots_from_2025.js
@@ -1,17 +1,14 @@
-let rcDataPath = "assets/data/rc/from_2024";
+let rcDataPath = "assets/data/rc/from_2025";
 
 $(document).ready(function () {
     resize();
-    generateRCPiePlot(`${rcDataPath}/20251217FasrcPisDeptSocSciUsageLab2024.tsv`, '.fassocrccpuxdept2024', "Dept", "CpuHours");
-    generateRCPiePlot(`${rcDataPath}/20251217FasrcPisDeptSocSciUsageLab2024.tsv`, '.fassocrcgpuxdept2024', "Dept", "GpuHours")
-    generateRCPiePlot(`${rcDataPath}/20251217FasrcPisDeptSocSciUsageLab2024.tsv`, '.fassocrcexecxdept2024', "Dept", "Jobs")
-    generateRCPiePlot(`${rcDataPath}/20251217FasrcPisDeptSocSciUsers2024.tsv`, '.fasssrc2024', "Dept", "Users");
-    generateRCPiePlot(`${rcDataPath}/20251217FasrcPisDeptSocSciPis.tsv`, '.fasrcpisxdept2024', "Dept", "Pis");
-    generateRCLinePlot(`${rcDataPath}/20251217FasrcPisDeptSocSciUsers.tsv`, '.fasrcsocresearcherxdeptxyear', "Dept", "Users");
-    generateRCLinePlot(`${rcDataPath}/20251217FasrcPisDeptSocSciCpuHours.tsv`, '.fasrcsoccomptimexdeptxyear', "Dept", "Cpu hours");
+    generateRCPiePlot(`${rcDataPath}/cpu.tsv`, '.fassocrccpuxdept2024', "Dept", "Num");
+    generateRCPiePlot(`${rcDataPath}/gpu.tsv`, '.fassocrcgpuxdept2024', "Dept", "Num")
+    generateRCPiePlot(`${rcDataPath}/jobs.tsv`, '.fassocrcexecxdept2024', "Dept", "Num")
+    generateRCPiePlot(`${rcDataPath}/dept.tsv`, '.fasssrc2024', "Dept", "Num");
 });
 
-function generateRCPiePlot(path, div, id, size) {
+function generateRcPiePlotFrom2025(path, div, id, size) {
 
     d3.tsv(path, function (data) {
         data.forEach(function (d) {
